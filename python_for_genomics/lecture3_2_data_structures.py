879c015b4d604196e92e89a9d80ad52017402844
brambloemen <brambloemen@outlook.com>
2021-05-13 17:51:07 +0200

    lecture 3.2 python data structures

---
+++ python_for_genomics/lecture3_2_data_structures.py
@@ -0,0 +1,37 @@
+#!/Anaconda/python  # in unix, put this in front to run from terminal
+"""
+tuples: store multiple elements in single variable
+"""
+t = 1, 2, 3
+print(t)
+t = (1, 2, 3)
+print(t)
+
+"""
+ sets: unordered collection without duplicates
+"""
+brca1 = {'DNA repair', 'zinc ion binding', 'etc...', 'DNA repair'}
+print(brca1)
+
+brca2 = {"string1", "string2", 'zinc ion binding', "DNA repair"}
+
+brca1 | brca2  # union operation
+brca1 & brca2  # intersection
+brca1 - brca2  # only elements in brcaa1 (difference)
+
+"""
+dictionaries: key-value pairs to look things up later
+"""
+tf_motif = {
+    'SP1': 'gggcgg',
+    'C/EBP': 'attgcgcaat'
+}
+
+# use square brackets to access tf_motif value by its key
+print("the recognition sequence motif of SP1 is %s." % tf_motif['SP1'])
+
+# assing new key to dictionary
+tf_motif['AP-1'] = 'tgagtca'
+
+# modify dictionary
+tf_motif['AP-1'] = 'tga(g/c)tca'
