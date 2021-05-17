c70eba1667715d5513c66a2207f15f9f9fce0fce
brambloemen <brambloemen@outlook.com>
2021-05-17 21:14:23 +0200

    finish biopython

--- python_for_genomics/lecture8_biopython.py
+++ python_for_genomics/lecture8_biopython.py
@@ -9,9 +9,18 @@ result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string)
 
 from Bio.Blast import NCBIXML
 blast_record = NCBIXML.read(result_handle)
-print(len(blast_record))
+print(len(blast_record.alignments))
 
 E_VALUE_THRESH = 0.01
 for alignment in blast_record.alignments:
     for hsp in alignment.hsps:
-        if hsp.expect < E_VALUE_THRESH
+        if hsp.expect < E_VALUE_THRESH:
+            print("""
+            ****alignment****
+            """)
+            print('sequence: ', alignment.title)
+            print('length: ', alignment.length)
+            print('e-value: ', hsp.expect)
+            print(hsp.query)
+            print(hsp.match)
+            print(hsp.sbjct)
